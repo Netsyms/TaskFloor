14640caaeeb3db292ed4d6269eda71c6c4fc151e
Skylar Ittner <admin@netsyms.com>
2017-05-13 14:31:01 -0600

    cleanup

--- settings.template.php
+++ settings.template.php
@@ -47,17 +47,6 @@ define('RECAPTCHA_SECRET_KEY', '');
 // See lang folder for language options
 define('LANGUAGE', "en_us");
 
-// Minimum length for new passwords
-// The system checks new passwords against the 500 worst passwords and rejects
-// any matches.
-// If you want to have additional password requirements, go edit action.php.
-// However, all that does is encourage people to use the infamous 
-// "post-it password manager".  See also https://xkcd.com/936/ and
-// http://stackoverflow.com/a/34166252/2534036 for reasons why forcing passwords
-// like CaPs45$% is not actually a great idea.
-// Encourage users to use 2-factor auth whenever possible.
-define("MIN_PASSWORD_LENGTH", 8);
-
 //////////////////////////////////////////////////////////////
 //  /!\       Warning: Changing these values may       /!\  //
 //  /!\  violate the terms of your license agreement!  /!\  //
