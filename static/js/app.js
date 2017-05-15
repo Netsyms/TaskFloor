16d9481abd4b75611b529fd4e1572e50bb0cd6a8
Skylar Ittner <admin@netsyms.com>
2017-05-15 11:45:46 -0600

    Change navbar color, add JS to remove &msg=xyz from URL, pngcrush images, update material-color to v1.2

--- static/js/app.js
+++ static/js/app.js
@@ -13,3 +13,18 @@ $(document).ready(function () {
     /* Activate tooltips */
     setupTooltips();
 });
+
+/*
+ * Remove feedback params from the URL so they don't stick around too long
+ */
+function getniceurl() {
+    var url = window.location.search;
+    url = url.substring(url.lastIndexOf("/") + 1);
+    url = url.replace(/&?msg=([^&]$|[^&]*)/i, "");
+    return url;
+}
+try {
+    window.history.replaceState("", "", getniceurl());
+} catch (ex) {
+    
+}
